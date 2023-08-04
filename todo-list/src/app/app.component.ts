76335b28611e018c6ee8bea393d57e23fa8e17fa
KiraHigashikata <brynjarfaune@gmail.com>
2023-08-05 01:34:04 +0200

    To-do list created

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+interface Task {
+  name: string;
+  content: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  newTask: Task = {
+    name: '',
+    content: '',
+    completed: false
+  };
+
+  tasksList: Task[] = [
+    {
+      name: 'Example',
+      content: 'description',
+      completed: false
+    }
+  ]
+
+  clearList = () => {
+    this.tasksList = [];
+  }
+  
+  addItem = (empty: boolean) => {
+    if (!empty) {
+      this.newTask.name = this.newTask.name.trim();
+      this.newTask.content = this.newTask.content.trim();
+      
+      if (!this.newTask.name) return false;
+      
+      this.tasksList.push(this.newTask);
+    }
+    this.newTask.name = '', this.newTask.content = '', this.newTask.completed = false;
+    return true;
+  }
+
+  removeItem = (item: Task) => {
+    item.name = item.name.trim();
+    if (!item.name) return false;
+
+    this.tasksList = this.tasksList.filter(t => t !== item);
+
+    return true;
+  }
+
 }
